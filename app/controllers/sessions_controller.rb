c8bd54c153c85463422315873b1f808febb9c47a
arnav-goel10 <arnav.goel1@outlook.com>
2024-01-15 14:09:04 +0800

    added 24 hour time limit on jwts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       payload = { user_id: user.id }
-      jwt = JWT.encode(payload, Rails.application.secrets.secret_key_base)
+      jwt = JWT.encode(payload, Rails.application.secrets.secret_key_base,exp = 24.hours.from_now)
       render json: { jwt: jwt }, status: :created
     else
       render json: { error: 'Invalid email or password' }, status: :unauthorized
